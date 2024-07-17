6eb9f96ecea814e757fd731352fdff44aca05ea4
aysemacam <aysema584@gmail.com>
2024-07-17 21:27:30 +0300

    inital commit

---
+++ calculator_app/calculator_app/calculator_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_appApp.swift
+//  calculator_app
+//
+//  Created by Akçay Holding on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct calculator_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
